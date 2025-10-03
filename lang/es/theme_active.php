d7741c0de1be04e4baebf33484ee7c4c34c07973
jrangelardila <jrangelardila@gmail.com>
2025-10-03 10:27:56 -0500

    Created file spanish, for lang

---
+++ lang/es/theme_active.php
@@ -0,0 +1,95 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     theme_active
+ * @category    string
+ * @copyright   2024 Jhon Rangel <jrangelardila@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'Tema Active';
+$string['pluginname'] = 'Tema Active';
+$string['loginbackground'] = "Imagen de fondo del inicio de sesión";
+$string['loginbackgrounddesc'] = "La imagen utilizada como fondo en la página de inicio de sesión.";
+$string['left'] = 'Izquierda';
+$string['right'] = 'Derecha';
+$string['center'] = 'Centro';
+$string['loginalignment'] = 'Alineación del inicio de sesión';
+$string['loginalignment_desc'] = 'Indicación de la alineación del inicio de sesión.';
+$string['maintenance_desc'] = 'Modo de mantenimiento';
+$string['configtitle'] = 'Configuraciones del tema Active';
+$string['loginsettings'] = 'Ajustes de inicio de sesión';
+$string['headersettings'] = 'Ajustes de cabecera';
+$string['footersettings'] = 'Ajustes de pie de página';
+$string['logo_settings'] = 'Logo del sitio';
+$string['logo_settingsdesc'] = 'Logo de la institución';
+$string['logocompact_settings'] = 'Logo compacto del sitio';
+$string['facebook_page'] = 'Página de Facebook';
+$string['instagram_page'] = 'Página de Instagram';
+$string['tiktok_page'] = 'Página de TikTok';
+$string['page_desc_footer'] = 'Página de la institución';
+$string['x_page'] = 'Página de X';
+$string['youtube_page'] = 'Página de YouTube';
+$string['linkedin_page'] = 'Página de LinkedIn';
+$string['footerbgcolor'] = 'Color de fondo';
+$string['footerbgcolor_desc'] = 'Selecciona una clase de fondo Bootstrap para aplicar en el pie de página.';
+$string['bg_primary'] = 'Primario (azul)';
+$string['bg_secondary'] = 'Secundario (gris)';
+$string['bg_success'] = 'Éxito (verde)';
+$string['bg_danger'] = 'Peligro (rojo)';
+$string['bg_warning'] = 'Advertencia (amarillo)';
+$string['bg_info'] = 'Información (teal)';
+$string['bg_light'] = 'Claro (blanco/gris)';
+$string['bg_dark'] = 'Oscuro (negro)';
+$string['follow_social_media'] = 'Síguenos';
+$string['contact_us'] = 'Contáctanos';
+$string['institutionaddress'] = 'Dirección de la institución';
+$string['institutionaddress_desc'] = 'Dirección física de la institución. Se mostrará en el pie de página.';
+$string['emailcontact'] = 'Correo institucional';
+$string['emailcontact_desc'] = 'Correo electrónico principal de contacto de la institución. Se mostrará en el pie de página.';
+$string['pbxcontact'] = 'PBX de la institución';
+$string['pbxcontact_desc'] = 'Número de PBX o teléfono principal de la institución. Se mostrará en el pie de página.';
+$string['whatsappcontact'] = 'WhatsApp institucional';
+$string['whatsappcontact_desc'] = 'Número oficial de WhatsApp de la institución. Se mostrará en el pie de página y se podrá usar para comunicación directa.';
+$string['whatsaap_hi'] = '¡Buenos días!';
+$string['about_us'] = 'Sobre nosotros';
+$string['footerlogo'] = 'Logo del pie de página';
+$string['footerlogo_desc'] = 'Sube un logo para mostrar en el pie de página del sitio.';
+$string['footerdescription'] = 'Descripción del pie de página';
+$string['footerdescription_desc'] = 'Contenido HTML personalizado que aparecerá en el pie de página. Puede usarse para una breve descripción, eslogan u otra información relevante sobre la institución.';
+$string['customlinks'] = 'Enlaces personalizados';
+$string['customlinks_desc'] = 'Agrega uno o más enlaces en formato JSON. Cada elemento debe incluir un nombre y una URL. Ejemplo:
+[
+    {"name": "YouTube", "url": "https://youtube.com"},
+    {"name": "GitHub", "url": "https://github.com"}
+]';
+$string['fontcolorfooter'] = 'Color de fuente del pie de página';
+$string['fontcolorfooter_desc'] = 'Selecciona el color de la fuente que se usará en el pie de página.';
+$string['navbarbgcolor'] = 'Color de fondo de la barra de navegación';
+$string['navbarbgcolor_desc'] = 'Selecciona el color de fondo para la barra de navegación.';
+$string['colorsettings'] = 'Ajustes de colores';
+$string['primarycolor'] = 'Color primario';
+$string['primarycolor_desc'] = 'El color principal de la marca utilizado en todo el tema.';
+$string['secondarycolor'] = 'Color secundario';
+$string['secondarycolor_desc'] = 'Un color complementario usado para resaltar o acentos.';
+$string['development_btn'] = '<i class="fa-solid fa-code"></i> Desarrollo para <a target="_blank" href="https://moodle.com/">&nbsp;Moodle</a>';
+$string['versioninfo'] = '<i class="fa-solid fa-code-compare"></i> Versión {$a->release}';
+$string['support_site'] = '<a href="{$a->url}"> <i class="fa-solid fa-headset"></i> Contactar soporte del sitio</a>';
